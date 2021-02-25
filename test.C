e7e5bd6306ff1bb6538e755d6f1a1c641e76e6ba
Daniel Schwen <daniel@schwen.de>
2021-02-25 16:56:01 -0700

    define dual number type

--- test.C
+++ test.C
@@ -6,11 +6,21 @@
 #include <iostream>
 
 // MetaPhysicL includes
+#include "metaphysicl/dualnumberarray.h"
+
+// dual number type
+constexpr int ad_size = 10;
+typedef double Real;
+typedef MetaPhysicL::NumberArray<ad_size, Real> DNDerivativeType;
+typedef MetaPhysicL::DualNumber<Real, DNDerivativeType, /*allow_skiping_derivatives=*/true> DualReal;
 
 // Eigen includes
 #include "Eigen/Core"
 
 int main(int argc, char* argv[])
 {
+  DualReal a = 10;
+  DualReal b = 5;
+  std::cout << a * b << '\n';
   return 0;
 }
