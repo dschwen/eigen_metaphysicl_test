73803075bd5a5d2ca9f8a7b9f6c9431b1192683d
Daniel Schwen <daniel@schwen.de>
2021-03-01 10:34:23 -0700

    trying to move the is_convertible specialization before teh Eigen includes

--- test1.C
+++ test1.C
@@ -4,6 +4,7 @@
 //
 
 #include <iostream>
+#include <type_traits>
 
 // MetaPhysicL includes
 #include "metaphysicl/dualnumberarray.h"
@@ -14,12 +15,33 @@ typedef double Real;
 typedef MetaPhysicL::NumberArray<ad_size, Real> DNDerivativeType;
 typedef MetaPhysicL::DualNumber<Real, DNDerivativeType, /*allow_skiping_derivatives=*/true> DualReal;
 
+// this should be defined in Macros.h ! Argh!
+#define EIGEN_ALIGN_TO_BOUNDARY(n) alignas(n)
+
+// macros and traits required for Eigen forwards (not not alphabetically sort these includes!)
+// clang-format off
+#include "Eigen/src/Core/util/Macros.h"
+#include "Eigen/src/Core/util/Constants.h"
+#include "Eigen/src/Core/util/Meta.h"
+#include "Eigen/src/Core/util/ForwardDeclarations.h"
+// clang-format on
+
+// define dual number based matrix type
+typedef Eigen::Matrix<DualReal, 6, 6> DualMatrix;
+
+// custom type trait
+namespace std
+{
+template <>
+struct is_convertible<DualMatrix, DualReal>
+{
+  static constexpr bool value = false;
+};
+} // namespace std
+
 // Eigen includes
 #include "Eigen/Core"
 #include "Eigen/Eigenvalues"
-
-// define dual number based matrix type
-typedef Eigen::Matrix<DualReal, 6, 6, Eigen::DontAlign> DualMatrix;
 
 int main(int argc, char* argv[])
 {
